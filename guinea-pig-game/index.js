7d908730911ed7802a05d44e02ffc54b36420bdc
sohdonald <sohdonald@myUbuntu.myguest.virtualbox.org>
2023-03-27 16:27:13 -0400

    added some practice

--- guinea-pig-game/index.js
+++ guinea-pig-game/index.js
@@ -84,3 +84,6 @@ function (e) {
         movePiggyDown();
     }
 });
+
+dog||cat
+
---
+++ guinea-pig-game/practice.js
@@ -0,0 +1,25 @@
+//and and ||
+const dog = true;
+const cat = false;
+
+dog||cat 
+
+//if dog = true, the dog value gets returned
+//if dog = false, the cat value gets returned
+
+dog&&cat
+
+//if dog = false, the dog value gets returned
+//if dog = true, the cat value gets returned
+
+//dog catches cat
+console.log(dog||cat);
+
+console.log(dog&&cat);
+
+console.log(cat||dog);
+
+console.log(cat&&dog);
+
+
+
